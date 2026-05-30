bc18213f3d012f07b75ef6c1eb3fa26d58cba3b2
SAM <sam@example.local>
2026-05-30 21:58:00 +0800

    feat: add policy routing dry run plan

--- migate/routing/__init__.py
+++ migate/routing/__init__.py
@@ -1,7 +1,19 @@
 from migate.routing.leak_guard import EgressGuardDecision, EgressGuardState, evaluate_egress_guard
+from migate.routing.policy_plan import (
+    PolicyRoutingDryRunResult,
+    PolicyRoutingDryRunStep,
+    PolicyRoutingPlan,
+    build_policy_routing_plan,
+    dry_run_policy_routing_plan,
+)
 
 __all__ = [
     "EgressGuardDecision",
     "EgressGuardState",
+    "PolicyRoutingDryRunResult",
+    "PolicyRoutingDryRunStep",
+    "PolicyRoutingPlan",
+    "build_policy_routing_plan",
+    "dry_run_policy_routing_plan",
     "evaluate_egress_guard",
 ]
